c5bd4b7c48f8f66dfc4a1fde7f1e766799c6035c
Israel Shedrack <israelshedrack913@gmail.com>
2025-01-24 15:35:36 +0100

    feat: created paystack service, added view for generating authorized flutter wave url

--- api/flutterwave.py
+++ api/flutterwave.py
@@ -1,5 +1,10 @@
 from django.conf import settings
 import requests
+from uuid import uuid4
+
+
+def generate_id():
+    return uuid4().hex
 
 class FlutterwaveService:
     secret_key = settings.FLUTTERWAVE_SECRET_KEY
@@ -11,14 +16,24 @@ class FlutterwaveService:
             "Content_Type" : "application/json"
         }
 
+        new_id = generate_id()
+
         data = {
             "amount" : amount, 
-            "email" : email
+            "email" : email, 
+            "tx_ref" : new_id, 
+            "redirect_url" : "http://localhost:5173/", 
+            "currency" : "NGN", 
+            "customer": {
+            "email": "israelshedrack913@gmail.com"
+        }
         }
 
         response = requests.post(
-            f"{self.base_url}/v3/transaction/initialize", json=data, headers=headers
+            f"{self.base_url}/v3/payments", json=data, headers=headers
         )
+
+        print(response.json())
 
         return response.json()
 
