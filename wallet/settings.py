9e452e61bb6302f4a16612328fa4b913cb6c3283
Israel Shedrack <israelshedrack913@gmail.com>
2025-01-27 12:09:18 +0100

    update: new changes to settings.py

--- wallet/settings.py
+++ wallet/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-9dsl+#h!!8bvi7vvs0hryauaxrl06x5!g#f-7(y73kciobg2hx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["wallet-api-6med.onrender.com", "https://wallet-rho-seven.vercel.app/"]
+ALLOWED_HOSTS = ["wallet-api-6med.onrender.com", "https://wallet-rho-seven.vercel.app/", "*"]
 
 
 # Application definition
